6139f4109fa8ae2fd0d7d8cdbf68333aa5d97cbd
Phoenix <holaux@gmail.com>
2016-11-30 21:32:07 +0800

    初始化

---
+++ Weibo-Pattern/Weibo-Pattern/Classes/View/Base/HOBaseTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Weibo-Pattern
+//
+//  Created by Phoenix on 30/11/2016.
+//  Copyright © 2016 holaux. All rights reserved.
+//
+
+import UIKit
+
+class HOBaseTabBarController: UITabBarController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+
+}
+
