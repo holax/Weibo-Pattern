7f0fa1be00394740c9c1507de2785bedb8664207
Phoenix <holaux@gmail.com>
2016-11-30 20:10:18 +0800

    项目准备

---
+++ Weibo-Pattern/Weibo-Pattern/HOTabBarController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weibo-Pattern
+//
+//  Created by Phoenix on 30/11/2016.
+//  Copyright © 2016 holaux. All rights reserved.
+//
+
+import UIKit
+
+class HOTabBarController: UITabBarController
+{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
